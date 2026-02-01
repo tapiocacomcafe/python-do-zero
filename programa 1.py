b7fd328708fcd1c94eabee2cb02b7282599b39c7
🦇Tapioca com café🦇 <andersonherminio16@gmail.com>
2026-02-01 03:50:53 -0300

    Add files via upload

---
+++ programa 1.py
@@ -0,0 +1,9 @@
+#User escolhe um número:
+n1 = int(input('Digite um numero: '))
+#Programa calcula o sucessor e o antecessor:
+su = n1 + 1
+
+ant = n1 - 1
+
+#Programa mostra o resultado na tela:
+print('O sucessor é: {}, e o antecessor é: {}'.format(su, ant))
